ed306231d42a01089b3e40c9ff384e07acf7c9d0
GowthamRaj.K <43011442+gowthamrajk@users.noreply.github.com>
2021-04-12 19:38:20 +0530

    Update mainpage.js

--- js/mainpage.js
+++ js/mainpage.js
@@ -3,8 +3,8 @@ function requestUserRepos()
     const gitHubForm = document.getElementById('gitHubForm');
     let usernameInput = document.getElementById('usernameInput');
     let gitHubUsername = usernameInput.value;          
-    console.log("repos");
     const xhr = new XMLHttpRequest();
+    try{
     const url = `https://api.github.com/users/${gitHubUsername}/repos`;
     xhr.open('GET', url, true);
     xhr.onload = function () 
@@ -15,17 +15,30 @@ function requestUserRepos()
         {
             let ul = document.getElementById('userRepos');
             let li = document.createElement('li');
-            li.classList.add('list-group-item')
-            li.innerHTML = (`
-                <p><strong>Repo:</strong> ${data[i].name}</p>
-                <p><strong>Description:</strong> ${data[i].description}</p>
-                <p><strong>Major Language:</strong> ${data[i].language}</p>
-                <p><strong>URL:</strong> <a href="${data[i].html_url}">${data[i].html_url}</a></p>
-                <p><strong>Size of Repository:</strong> ${data[i].size}</p>
-                <p><strong>Forks done:</strong> ${data[i].forks_count}</p>
+            li.classList.add('list-group-item');
+            if(data.message === "Not Found")
+            {
+                $('#userRepos').empty();
+                li.innerHTML = (`<p><strong><font color="red"><center>No User available in this Name, Please Try with different User Name !!!</center></font></strong></p>`);
+            }
+            else
+            {
+                li.innerHTML = (`
+                    <p><strong>Repo:</strong> ${data[i].name}</p>
+                    <p><strong>Description:</strong> ${data[i].description}</p>
+                    <p><strong>Major Language:</strong> ${data[i].language}</p>
+                    <p><strong>URL:</strong> <a href="${data[i].html_url}">${data[i].html_url}</a></p>
+                    <p><strong>Size of Repository:</strong> ${data[i].size}</p>
+                    <p><strong>Forks done:</strong> ${data[i].forks_count}</p>
                 `);
+            }
             ul.appendChild(li);
         }
+    }
+    }
+    catch(err)
+    {
+        console.log("error");
     }
     xhr.send();
 }
@@ -36,16 +49,23 @@ function requestUserInfo()
     let usernameInput = document.getElementById('usernameInput');
     let gitHubUsername = usernameInput.value;          
     const xhr = new XMLHttpRequest();
+    try{
     const url = `https://api.github.com/users/${gitHubUsername}`;
     xhr.open('GET', url, true);
     xhr.onload = function () 
     {
         const data = JSON.parse(this.response);
-        console.log(data);
         let ul = document.getElementById('userRepos');
         let li = document.createElement('li');
+        li.classList.add('list-group-item');
+        if(data.message === "Not Found")
+        {
+            $('#userRepos').empty();
+            li.innerHTML = (`<p><strong><font color="red"><center>No User available in this Name, Please Try with different User Name !!!</center></font></strong></p>`);
+        }
+        else
+        {
         $('#userRepos').empty();
-        li.classList.add('list-group-item')
         li.innerHTML = (`
             <p><a href="${data.html_url}"><img class="img-thumbnail" width="200" height="200" src="${data.avatar_url}"/></a></p>
             <p><strong>Name:</strong> ${data.name}</p>
@@ -61,7 +81,13 @@ function requestUserInfo()
             <p><strong>Twitter ID:</strong> ${data.twitter_username}</p>
             <p><strong>Profile URL:</strong> <a href="${data.html_url}">${data.html_url}</a></p>
             `);
+        }
         ul.appendChild(li);
+    }
+    }
+    catch(err)
+    {
+        console.log("error");
     }
     xhr.send();
 }
